a1e9a3d28a542b86c810112f5efb87e0167251e8
Gregory Liras <gregliras@gmail.com>
2012-02-11 23:41:09 +0200

    compliant code

--- weather.py
+++ weather.py
@@ -3,8 +3,12 @@
 from urllib2 import Request, urlopen
 from re import search
 
-req = Request("http://www.rssweather.com/wx/gr/athinai/rss.php")
-response = urlopen(req)
-the_page = response.read()
-result = search('Weather ::.*?<',the_page)
-print result.group(0)[:len(result.group(0))-1]
+def main(): 
+    req = Request("http://www.rssweather.com/wx/gr/athinai/rss.php")
+    response = urlopen(req)
+    the_page = response.read()
+    result = search('Weather ::.*?<',the_page)
+    print result.group(0)[:len(result.group(0))-1]
+
+if __name__=="__main__":
+    main()
